71d486b5495a398eba2ccdd1a7d18345cf27a68a
Muhammad Owais Mazhar <100398204+owais-mazhar@users.noreply.github.com>
2024-10-11 15:46:49 +0000

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from mlproject2 import logger
+from mlproject2.pipeline.stage_01_data_ingestion import DataIngestionTrainigPipeline
 
 
-logger.info("Welcome to our custom logging")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
+    data_ingestion = DataIngestionTrainigPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<\n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/mlproject2/components/data_ingestion.py
@@ -0,0 +1,29 @@
+import os
+import urllib.request as request
+import zipfile
+from mlproject2 import logger
+from mlproject2.utils.common import get_size
+from pathlib import Path
+from mlproject2.entity.config_entity import DataIngestionConfig
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, headers = request.urlretrieve(
+                url = self.config.source_URL,
+                filename = self.config.local_data_file
+            )
+            logger.info(f"{filename} downloaded! with the following info: \n{headers}")
+        else:
+            logger.info(f"{filename} already exists of size: {get_size(Path(self.config.local_data_file))}")
+
+
+    def extract_zip_file(self):
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
